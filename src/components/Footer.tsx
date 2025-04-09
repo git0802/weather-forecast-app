f69631b1d4306fd2feaf55879cacf708382d3a6c
git0802 <t2k20802@gmail.com>
2025-04-09 19:38:08 +0530

    footer padding corrected

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="bg-white dark:bg-black w-full text-xs font-light mt-6 lg:text-sm">
-      <div className="flex justify-between py-2 px-3  m-auto sm:max-w-[640px] lg:max-w-[1024px] xl:max-w-[1280px]">
+      <div className="flex justify-between py-4 px-4 m-auto sm:max-w-[640px] lg:max-w-[1024px] xl:max-w-[1280px]">
         <p className="">&copy; 2024 Next Forecast. All rights reserved.</p>
         <Link
           href="https://sujalvanjare.vercel.app"
