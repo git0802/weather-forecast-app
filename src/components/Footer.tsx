9319e42e7018cc393b224b2b1818f618f25908b5
LoveBee <lovebee0802@gmail.com>
2025-05-16 01:24:02 -0400

    update footer copyright year and attribution link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,13 @@ export default function Footer() {
   return (
     <footer className="bg-white dark:bg-black w-full text-xs font-light mt-6 lg:text-sm">
       <div className="flex justify-between py-4 px-4 m-auto sm:max-w-[640px] lg:max-w-[1024px] xl:max-w-[1280px]">
-        <p className="">&copy; 2024 Next Forecast. All rights reserved.</p>
+        <p className="">&copy; 2025 GungXu. All rights reserved.</p>
         <Link
-          href="https://sujalvanjare.vercel.app"
+          href="https://lovebee.me"
           target="_blank"
           className="hover:text-purple-500  hover:underline transition ease-linear duration-200"
         >
-          made by Sujal
+          Made by git0802
         </Link>
       </div>
     </footer>
